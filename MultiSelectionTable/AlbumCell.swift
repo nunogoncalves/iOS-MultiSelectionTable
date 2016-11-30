586ff8b5e873f7407665a2a9fb11d32ee55baeab
Nuno Gonçalves <nunogoncalves@Nunos-MBP.lan>
2016-11-30 15:12:36 +0000

    AlbumCell added

--- MultiSelectionTable/AlbumCell.swift
+++ MultiSelectionTable/AlbumCell.swift
@@ -2,16 +2,24 @@
 //  AlbumCell.swift
 //  MultiSelectionTable
 //
-//  Created by Nuno Gonçalves on 28/11/16.
+//  Created by Nuno Gonçalves on 30/11/16.
 //  Copyright © 2016 Nuno Gonçalves. All rights reserved.
 //
 
 import UIKit
 
-class AlbumCell: UITableViewCell {
 
+class AlbumCell: UITableViewCell {
+    
     @IBOutlet weak var albumImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
     
+    @IBOutlet weak var bottomLineHeightConstraint: NSLayoutConstraint!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        bottomLineHeightConstraint.constant = 0.5
+    }
 }
