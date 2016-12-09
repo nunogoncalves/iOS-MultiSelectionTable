2b21b12a5eab199581d5ff65a578a8887e1ae352
Nuno Gonçalves <numicago@gmail.com>
2016-12-09 11:48:01 +0000

    Adding bridging header

---
+++ Example/MultiSelectionTable/BridgingHeader.h
@@ -0,0 +1,15 @@
+//
+//  BridgingHeader.h
+//  MultiSelectionTable
+//
+//  Created by Nuno Gonçalves on 09/12/16.
+//  Copyright © 2016 Nuno Gonçalves. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+
+#endif /* BridgingHeader_h */
+
+#import <CommonCrypto/CommonCrypto.h>
